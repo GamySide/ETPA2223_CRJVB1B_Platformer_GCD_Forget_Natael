e8624c961764c544ab455ef5e81027e8b1886ce7
Game <nataelforget@gmail.com>
2023-05-12 13:10:47 +0200

    .

--- code/niveau_1.js
+++ code/niveau_1.js
@@ -5,17 +5,17 @@ export default class Niveau1 extends Phaser.Scene {
     init(data) {
     }
     preload() {
-        console.log("test");
         this.load.image("tileset", "../asset_lvl1/placholder_sol.png");
         this.load.image("fond1", "../asset_lvl1/fond_niveau_1.png");
         this.load.image("fond2", "../asset_lvl1/fond_niveau_1(2).png");
         this.load.tilemapTiledJSON("map", "../asset_lvl1/map_lvl1.json");
     }
     create() {
-        this.physics.world.setBounds(0, 0, 1600, 1600);
+        this.physics.world.setBounds(20*256, 0*256, 207*256, 30*256);
         const level1 = this.add.tilemap("map");
         const tileset = level1.addTilesetImage("placholder_sol", "tileset");
         const platform = level1.createLayer("platform", tileset);
+        console.log("test");
 
     }
     update() { }
