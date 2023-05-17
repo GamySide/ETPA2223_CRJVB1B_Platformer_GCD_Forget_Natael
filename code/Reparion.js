1fcfd9ebce16361256aa29d81e3998642fd2759d
JonathanFichant <nataelforget@gmail.com>
2023-05-17 10:46:33 +0200

    .

--- code/Reparion.js
+++ code/Reparion.js
@@ -1,65 +1,31 @@
-export default class Reparion extends Phaser.Character{
-    constructor() {
-        super('Reparion');
+export class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, sprite) {
+        super(scene, x, y, sprite);
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+        this.init();
+        this.initEvents();
     }
-    init(data) {
-    }
-    preload() {
-        this.load.spritesheet('reparion', '../asset_lvl1/reparion.png',{ frameWidth: 256, frameHeight: 512 });
-    }
-    create() {
-        this.player = this.physics.add.sprite(4*256, 14*256, 'reparion');
-        this.player.setSize(256,512);
-        this.player.setOffset(0,0);
-        this.physics.add.collider(this.player, platform)
-        this.player.setCollideWorldBounds(true);
+    init() {
+        //Variable 
+        this.alive = true;
+        this.hp = 5;
+        this.hpMax = 5;
+        this.hplvl = 1;
+        this.atklvl = 1;
+        this.deflvl = 1;
+        this.dodgelvl = 1;
+        this.endurancelvl = 1;
+        this.reslvl = 1;
 
-        this.clavier = this.input.keyboard.addKeys('A,Z,E,R,Q,S,D,ENTER,ESC,SPACE');
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.pad = {
-            leftStick: { x: 0, y: 0 },
-            rightStick: { x: 0, y: 0 },
-            A: false,
-            B: false,
-            X: false,
-            Y: false,
-            L1: 0,
-            L2: 0,
-            R1: 0,
-            R2: 0,
-            right: false,
-            left: false,
-            up: false,
-            down: false,
-        }
-        this.cameras.main.startFollow(this.player);
-        this.cameras.main.setBounds(0*256, 0*256, 208*256, 20*256);
-        console.log("test");
-    }
-    update(){
-        if (this.clavier.Q.isDown && this.player.body.onFloor()) { 
-            this.player.setVelocityX(-768);
-            move = true; 
-        }
-        else if (this.clavier.D.isDown && this.player.body.onFloor()) { 
-            this.player.setVelocityX(768);   
-            move = true         
-        }
+        //Controle
+        this.cursors = this.scene.input.keyboard.createCursorKeys(); //Déplacement huit direction
+        this.keyA = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A); //Attaque CaC
+        this.keyZ = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z); //Attaque Distance
+        this.keyE = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E); //Parry
 
-        if (this.clavier.SPACE.isDown && this.clavier.Q.isDown && this.player.body.onFloor()) {
-            this.player.setVelocityX(-512);
-            this.player.setVelocityY(-1024);
-        }
-        else if (this.clavier.SPACE.isDown && this.clavier.D.isDown && this.player.body.onFloor()) {
-            this.player.setVelocityX(512);
-            this.player.setVelocityY(-1024);
-        }
-        else if(this.clavier.SPACE.isDown && this.player.body.onFloor()){
-            this.player.setVelocityY(-1024);
-        }
-        else if(this.player.body.onFloor() && move == false){
-            this.player.setVelocityX(0);
-        }
-        move = false;
+        //Parametre
+        this.setOrigin(0.5, 0.5)
+        this.setCollideWorldBounds(true);
     }
 }
